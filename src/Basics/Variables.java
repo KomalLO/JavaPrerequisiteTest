b68597604917e9989b77e3b11dd23f59528c3b89
KomalLO <komalchintu56@gmail.com>
2026-08-16 17:57:03 +0530

    Initial commit

---
+++ src/Basics/Variables.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double height = 6.0;
+        String name = "Harsha";
+        boolean student = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Student: " + student);
+    }
+}
